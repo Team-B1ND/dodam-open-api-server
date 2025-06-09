5d0b47c580c6e137e6025794976b2c729f11e841
kmg <legolove08@naver.com>
2025-06-09 23:42:09 +0900

    init

---
+++ src/main/kotlin/com/b1nd/dodamopenapi/DodamOpenApiApplication.kt
@@ -0,0 +1,11 @@
+package com.b1nd.dodamopenapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DodamOpenApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DodamOpenApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/b1nd/dodamopenapi/DodamOpenApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.b1nd.dodamopenapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DodamOpenApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
